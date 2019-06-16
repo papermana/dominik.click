83e5dc03c3df1245c22a13b44bced68ab6a57b4a
Dominik Rowicki <yzukonta@gmail.com>
2019-06-16 21:42:39 +0200

    Add custom content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,35 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Dominik Rowicki.</h1>
+    <p>A Front End Developer.</p>
+    <p>Focused on simple UX and clean and functional code.</p>
+    <hr />
+    <h2>❤︎s:</h2>
+    <ul>
+      <li>Javascript. (Yes!)</li>
+      <li>React.</li>
+      <li>Functional programming.</li>
+      <li>Clojure.</li>
+      <li>Structure and Interpretation of Computer Programs.</li>
+      <li>Git.</li>
+      <li>Simplicity.</li>
+    </ul>
+    <hr />
+    <h2>Some useful things:</h2>
+    <ul>
+      <li>
+        <a href="https://github.com/papermana/react-modern-sticky">
+          <code>react-modern-sticky</code>
+        </a>{" "}
+        &mdash; Lightweight sticky component for the modern web.
+      </li>
+    </ul>
   </Layout>
 )
 
