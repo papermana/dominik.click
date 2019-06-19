63aa2c608ae70faa36d58f492b61588c8c37bd6f
Dominik Rowicki <yzukonta@gmail.com>
2019-06-19 02:47:37 +0200

    Customize SEO metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Dominik Rowicki.`,
+    description: `Dominik Rowicki. A Front End Developer.`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
