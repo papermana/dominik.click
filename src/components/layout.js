4307b8eb3a88b8786fc9096e3ab4876f1864dbb7
Dominik Rowicki <yzukonta@gmail.com>
2019-06-20 16:02:46 +0200

    Add favicon

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,34 @@
 import React from "react"
 import PropTypes from "prop-types"
+import Helmet from "react-helmet"
+
+import favicon16 from "../images/favicon-16.png"
+import favicon32 from "../images/favicon-32.png"
+import favicon64 from "../images/favicon-64.png"
 
 import "./layout.css"
 
 const Layout = ({ children, className }) => (
-  <main className={className}>{children}</main>
+  <>
+    <Helmet
+      link={[
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: `${favicon16}`,
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: `${favicon32}`,
+        },
+        { rel: "shortcut icon", type: "image/png", href: `${favicon64}` },
+      ]}
+    />
+    <main className={className}>{children}</main>
+  </>
 )
 
 Layout.propTypes = {
